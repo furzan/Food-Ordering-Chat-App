3793537d41aa5c155d8fc1427c50557fe1eb3bc8
furzan <furzanahmed07@gmail.com>
2025-10-24 09:41:02 +0500

    adding env example

--- backend/app/agents/my_agents/assistant_agent.py
+++
@@ -1,8 +0,0 @@
-from agents import Agent
-from backend.app.agents.my_config.gemini_config import MODEL
-
-agent = Agent(
-    name = "assistant", 
-    instructions= "you are a helpfull assistant.",
-    model= MODEL
-    )
